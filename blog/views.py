8b162acc57cc1a7bb41a97bf8d5315f80c9c297a
Marty <goodmarty@gmail.com>
2016-06-16 16:25:26 +0800

    add publish button and drafts

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()   #not to publish now, but draft first.
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -44,3 +44,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+#def publish(self):
+#    self.published_date = timezone.now()
+#    self.save()
